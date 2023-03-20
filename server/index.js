967147066deac0ecfba6d6b3eb1e6ce7e22cc326
david karp <davidkarp94@gmail.com>
2023-03-20 20:40:56 -0300

    upd

--- server/index.js
+++ server/index.js
@@ -53,13 +53,6 @@ app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
-/* Server static assets if in production */
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
